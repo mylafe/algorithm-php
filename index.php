64f6454741ce7e2fdf4d16e03340f1d3b462b4c4
LiTao <1287862489@qq.com>
2020-02-21 15:19:16 +0800

    【新增】猴王算法

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 require_once 'mylafe/solution.php';
 
-//实例化
 $solution = new mylafe\Solution();
 
 $twoSum = $solution->getSum(1, 2);
 echo $twoSum . PHP_EOL;
+
 $twoSum = $solution->getCash(1);
 echo $twoSum . PHP_EOL;
+
+$monkeys = array(1 , 2 , 3 , 4 , 5 , 6 , 7, 8 , 9 , 10); //monkey的编号
+$m = 4; //数到第几只的那只猴子被踢出去
+$solution->killMonkey($monkeys , $m);
--- mylafe/solution.php
+++ mylafe/solution.php
@@ -13,8 +13,34 @@ class Solution
         return $a + $b;
     }
 
+    /**
+     * @param $a
+     * @return float|int
+     */
     public function getCash($a)
     {
         return 2 * $a;
+    }
+
+    /**
+     * @param $monkeys
+     * @param $m
+     * @param int $current
+     */
+    public function killMonkey($monkeys , $m , $current = 0){
+        $number = count($monkeys);
+        $num = 1;
+        if (count($monkeys) == 1) {
+            echo $monkeys[0]."成为猴王了";
+            return;
+        } else  {
+            while($num++ < $m){
+                $current++ ;
+                $current = $current%$number;
+            }
+            echo $monkeys[$current]."的猴子被踢掉了<br/>";
+            array_splice($monkeys , $current , 1);
+            $this->killMonkey($monkeys , $m , $current);
+        }
     }
 }
